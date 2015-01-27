551a917838b31a5280b4c61be29059c2dc10c5f5
Marissa <marissavega90@gmail.com>
2015-01-26 18:16:04 -0600

    Completed To-Do List with ability to check off items

--- public/todo_list_db.php
+++ public/todo_list_db.php
@@ -25,19 +25,17 @@ if (!empty($_POST)) {
 		empty($_POST['date']) ||
 		empty($_POST['priority'])
 	) {
-    	$errorMsg = 'Must fill out form completely!';
+    	echo 'Must fill out form completely!';
 	} else {
 
 		$query = "INSERT INTO to_do_list (to_do, date, priority)
 			VALUES (:to_do, :date, :priority)";
-			// VALUES (:email, :name)');
+			
 		$stmt = $dbc->prepare($query);
 
 		$stmt->bindValue(':to_do', $_POST['to_do'], PDO::PARAM_STR);
 	    $stmt->bindValue(':date',  $_POST['date'],  PDO::PARAM_STR);
-		$stmt->bindValue(':priority', $_POST['priority'], PDO::PARAM_STR);
-		// $stmt->bindValue(':complete', $_POST['complete'], PDO::PARAM_STR);
-		// $stmt->bindValue(':time_created', $_POST['time_created'], PDO::PARAM_STR);
+		$stmt->bindValue(':priority', $_POST['priority'], PDO::PARAM_INT);
 		$stmt->execute();
 	}
 }
@@ -47,10 +45,22 @@ if (isset($_GET['remove'])) {
 	$query = "DELETE FROM to_do_list
 			WHERE id = :id";
 	$stmt = $dbc->prepare($query);
-	$stmt->bindValue(':id', $id, PDO::PARAM_STR);
+	$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 
 	$stmt->execute();
 }
+
+if (isset($_GET['check'])) {
+	// $complete = 1;
+	$id = $_GET['check'];
+	$query = "UPDATE to_do_list SET complete = 1
+			WHERE id = :id";
+	$stmt = $dbc->prepare($query);
+	$stmt->bindValue(':id', $id);
+
+	$stmt->execute();
+}
+
 
 $nextprev = $dbc->query("SELECT COUNT(*) FROM to_do_list")->fetchColumn();
 
@@ -62,7 +72,7 @@ if(!isset($_GET['page'])) {
 
 $offset = ($page-1) * 10;
 
-$query = $dbc->prepare("SELECT id, to_do, date, priority, complete
+$query = $dbc->prepare("SELECT id, to_do, date, priority, complete, date_created
 				FROM to_do_list ORDER BY priority LIMIT 10 OFFSET :offset");
 $query->bindValue(':offset', $offset, PDO::PARAM_INT);
 $query->execute();
@@ -83,10 +93,11 @@ $list_items = $query->fetchAll(PDO::FETCH_ASSOC);
     <!-- Bootstrap -->
     <link href="css/to_do_bootstrap.min.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <style type="text/css">
 
     .input-long {
-    	width: 450px;
+    	width: 500px;
     }
 
     body {
@@ -104,7 +115,7 @@ $list_items = $query->fetchAll(PDO::FETCH_ASSOC);
 
 
 <div class="container">
-	<h1 align="center">To-Do List</h1>
+	<h1 class="text-center">To-Do List</h1>
 	<table class="table table-bordered">
 		<tr>
 
@@ -120,14 +131,22 @@ $list_items = $query->fetchAll(PDO::FETCH_ASSOC);
 		<? foreach ($list_items as $key => $entry): ?>
 
 			<tr>
-					<td><?= $entry['to_do']; ?></td>
-					<td><?= $entry['date']; ?></td>
-					<td><?= $entry['priority']; ?></td>
-					<td><?= $entry['complete']; ?></td>
-				
-				<td><? echo date('l jS \of F Y'); ?></td>
+				<td><?= $entry['to_do']; ?></td>
+				<td><?= date('l jS \of F Y', strtotime($entry['date'])); ?></td>
+				<td><?= $entry['priority']; ?></td>
+				<? if($entry['complete'] == 0): ?>
 
-				<td><a href="?remove=<?= $entry['id'] ?>"><button class="btn btn-danger">Delete</button></a></td>
+				<td><a href="todo_list_db.php?check=<?= $entry['id'] ?>" class="fa fa-square-o fa-2x"></a></td>
+				
+				<?php else: ?>
+
+				<td><a href="todo_list_db.php?check=<?= $entry['id'] ?>" class="fa fa-check-square-o fa-2x"></a></td>
+
+				<? endif ?>
+
+				<td><?= date('l jS \of F Y', strtotime($entry['date_created'])); ?></td>
+
+				<td><a href="todo_list_db.php?remove=<?= $entry['id'] ?>" class="btn btn-danger">Delete</a></td>
 			</tr>
 
 		<? endforeach; ?>
@@ -145,7 +164,7 @@ $list_items = $query->fetchAll(PDO::FETCH_ASSOC);
 
 			<input class="input-lg" type="text" id="date" name="date" placeholder="Date To-Do (YYYY-MM-DD)">
 
-			<input class="input-long input-lg" type="text" id="priority" name="priority" placeholder="Priority (1 = Highest priority 5= Lowest priority)">
+			<input class="input-long input-lg" type="text" id="priority" name="priority" placeholder="Priority (1 = Highest priority   5 = Lowest priority)">
 
 			<button class="btn btn-default" type="submit" name="submit">Submit</button>
 
@@ -156,23 +175,25 @@ $list_items = $query->fetchAll(PDO::FETCH_ASSOC);
 	
 
 </div>
-<nav>
-  	<ul class="pager">
+<div class="container">
+	<nav>
+	  	<ul class="pager">
 
-		<? if ($page > 1 ): ?>
-			
-		    <li class="previous"><a href="?page=<?= $page-1 ?>"><span aria-hidden="true">&larr;</span> Previous</a></li>
+			<? if ($page > 1 ): ?>
+				
+			    <li class="previous"><a href="?page=<?= $page-1 ?>"><span aria-hidden="true">&larr;</span> Previous</a></li>
 
-		<? endif; ?>
+			<? endif; ?>
 
-		<? if ($page <= $nextprev/10):?> 
+			<? if ($page <= $nextprev/10):?> 
 
-	    	<li class="next"><a href="?page=<?= $page+1 ?>">Next <span aria-hidden="true">&rarr;</span></a></li>
+		    	<li class="next"><a href="?page=<?= $page+1 ?>">Next <span aria-hidden="true">&rarr;</span></a></li>
 
-		<? endif; ?>
+			<? endif; ?>
 
-  	</ul>
-</nav>
+	  	</ul>
+	</nav>
+</div>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
